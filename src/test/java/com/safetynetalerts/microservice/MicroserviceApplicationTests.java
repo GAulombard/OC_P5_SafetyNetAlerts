b4ff5cbac4bbbbdada59968aa6330a495220fac1
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-05-15 10:51:51 +0200

    creation project

---
+++ src/test/java/com/safetynetalerts/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetalerts.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
