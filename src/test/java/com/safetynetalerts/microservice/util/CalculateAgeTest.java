6328bd4eff02e67eb705de3accae6e2e85c1496d
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-06-18 13:19:20 +0200

    updated pom file, modification CalculateAgeTest, added javadoc, added surefire and jacoco reports

--- src/test/java/com/safetynetalerts/microservice/util/CalculateAgeTest.java
+++ src/test/java/com/safetynetalerts/microservice/util/CalculateAgeTest.java
@@ -6,10 +6,15 @@ import com.safetynetalerts.microservice.DAO.MedicalRecordsDAO;
 import com.safetynetalerts.microservice.datasource.DataBase;
 import com.safetynetalerts.microservice.datasource.DataBaseManager;
 import com.safetynetalerts.microservice.datasource.DataBaseTestService;
+import com.safetynetalerts.microservice.model.MedicalRecords;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,13 +38,17 @@ public class CalculateAgeTest {
 
     @AfterEach
     void tearDown() {
-
+        //dataBaseTestService.clearDBTest();
+        //dataBaseTestService.restoreDBTest();
     }
 
     @Test
     void CalculateAgeTest_whenAdult() {
         CalculateAge ageCalculation = new CalculateAge();
-        int age = ageCalculation.calculate(medicalRecordsDAOImp.findByFirstAndLastName("firstName","lastName"));
+        Calendar calendar = new GregorianCalendar(1994,06,15);
+        Date date = calendar.getTime();
+        MedicalRecords medicalRecord = new MedicalRecords("firstName","lastName",date,null,null);
+        int age = ageCalculation.calculate(medicalRecord);
         assertEquals(27,age);
 
     }
@@ -47,7 +56,10 @@ public class CalculateAgeTest {
     @Test
     void CalculateAgeTest_whenChildren() {
         CalculateAge ageCalculation2 = new CalculateAge();
-        int age2 = ageCalculation2.calculate(medicalRecordsDAOImp.findByFirstAndLastName("firstName2","lastName2"));
+        Calendar calendar2 = new GregorianCalendar(2012,06,15);
+        Date date2 = calendar2.getTime();
+        MedicalRecords medicalRecord2 = new MedicalRecords("firstName2","lastName2",date2,null,null);
+        int age2 = ageCalculation2.calculate(medicalRecord2);
         assertEquals(9,age2);
     }
 
